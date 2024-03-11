7c97630e6e5ba4d98e0e518222dc5a9effe8e197
Novak Nikolic <155026416+92no@users.noreply.github.com>
2024-03-11 18:06:24 +0100

    Edit script.js
    Added strict mode and let to variables

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+"use strict";
+
 // Random number chooses rock, paper or scissors
 // Returns string rock, paper or scissors
 function getComputerChoice() {
   // Get Random number: 0, 1, 2
-  num = Math.floor(Math.random() * 3);
+  let num = Math.floor(Math.random() * 3);
   if (num === 0) {
     return "rock";
   } else if (num === 1) {
@@ -49,18 +51,18 @@ function playRound(playerSelection, computerSelection) {
 // Play a game of rock paper scissors, consisting of five rounds
 // Keeps track of the record
 function playGame() {
-  playerWin = 0;
-  playerLose = 0;
+  let playerWin = 0;
+  let playerLose = 0;
   // Play five rounds of rock paper scissors
-  for (i = 0; i < 5; i++) {
+  for (let i = 0; i < 5; i++) {
     // Get user choice as string
-    playerSelection = prompt("Rock, Paper or Scissors").toLowerCase();
+    let playerSelection = prompt("Rock, Paper or Scissors").toLowerCase();
 
     // Get computer choice
-    computerSelection = getComputerChoice();
+    let computerSelection = getComputerChoice();
 
     // Save output of a round in variable result
-    result = playRound(playerSelection, computerSelection);
+    let result = playRound(playerSelection, computerSelection);
     console.log(result);
 
     // Check if player wins or looses and track scores
